e4951b75fc5c42744473561afd3bbf1ede0add8d
PaulWalker <91833744+Vampire421@users.noreply.github.com>
2021-10-21 09:33:22 +0530

    Add files via upload

---
+++ LuciferMoringstar_Robot/__init__.py
@@ -0,0 +1,35 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+ഒത്തിരി help ഒന്നും ഇല്ല ആകെ രണ്ട് കാര്യമേ ഒള്ളൂ
+
+1. Add Me To Group
+2. Make As Admin 
+
+വെറുതെ കുതികൊണ്ട് ഇരികണ്ട 😏
+
+"""
+
+ABOUT = """
+➥ Name :  Auto Filter V1
+    
+➥ Editor : [Paulwalker](https://github.com/Vampire421)
+
+➥ Database : [MONGODB FREE TIER](https://mongodb.com)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Source Code : [Click Me](https://t.me/ippotharam34)
+"""
