e5580ee3e3083ef48f721c8be162f2fe0491f3c1
PaulWalker <91833744+Vampire421@users.noreply.github.com>
2021-10-21 08:24:39 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -119,7 +119,7 @@ async def group(client, message):
         nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
-            nyva=botusername.username
+            nyva=botusername.usernamehttps://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id
             BOT["username"]=nyva
         files = await get_filter_results(query=search)
         if files:
@@ -127,7 +127,10 @@ async def group(client, message):
                 file_id = file.file_id
                 filename = f"[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]
+                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}"),
+                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", url=f"https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]
+
+                )
                 )
         else:
             return
