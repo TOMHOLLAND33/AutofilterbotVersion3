2f12b6faf87d37f4c3f67b9b5b687e214a45a3ec
TOM HOLLAND <88812743+TOMHOLLAND33@users.noreply.github.com>
2021-10-23 17:42:00 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -23,15 +23,15 @@ HELP = """
 ABOUT = """
 ➥ 𝙽𝚊𝚖𝚎 : 𝚂𝚊𝚛𝚊 
     
-➥ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : [𝙿𝚊𝚞𝚕𝚆𝚊𝚕𝚔𝚎𝚛](https://github.com/Vampire421)
+➥ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : [TOM HOLLAND](https://t.me/TOM_HOLLA_ND)
 
-➥ Contribution : [Mufaz-tg](https://github.com/Mufaz-TG)
+➥ Contribution : [LOKI LAUFESON](https://t.me/Loki_Lau_feyson)
 
 ➥ 𝙳𝚊𝚝𝚊𝚋𝚊𝚜𝚎 : [𝙼𝚘𝚗𝚐𝚘𝙳𝙱](https://mongodb.com)
 
 ➥ 𝙻𝚊𝚗𝚐𝚞𝚊𝚐𝚎 : 𝙿𝚢𝚝𝚑𝚘𝚗 3 
 
-➥ 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : 𝙱𝙴𝚃𝙰 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 1.2
+➥ 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : 𝙱𝙴𝚃𝙰 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 3.1
 
-➥ 𝚂𝚘𝚞𝚛𝚌𝚎 𝙲𝚘𝚍𝚎 : [𝙲𝚕𝚒𝚌𝚔 𝙷𝚎𝚛𝚎 ❤️](https://github.com/Vampire421/-)
+➥ 𝚂𝚘𝚞𝚛𝚌𝚎 𝙲𝚘𝚍𝚎 : [𝙲𝚕𝚒𝚌𝚔 𝙷𝚎𝚛𝚎 ](https://t.me/chilapolinnukittiyalo)
 """
