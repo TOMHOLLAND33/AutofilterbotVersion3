8632e44467155f4947f141fd6f6a906d02da90c5
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-10-23 12:50:08 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -124,10 +124,11 @@ async def start(bot, message):
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[
-                InlineKeyboardButton("➕ Add to group ➕", url="http://t.me/fbm_autofilterbot?startgroup=true"),   
+                InlineKeyboardButton("➕ Add to group ➕", url="http://t.me/fbm_autofilterbot?startgroup=true")
+                ],[ 
                 InlineKeyboardButton("🔍 Search Here", switch_inline_query_current_chat='')
-                ],[
                 InlineKeyboardButton("❔ Help", callback_data="help"),
+                ],[
                 InlineKeyboardButton("🤖 Updates", url="https://t.me/housefull_links"),
                 InlineKeyboardButton("☺️ About", callback_data="about")
                 ]]
