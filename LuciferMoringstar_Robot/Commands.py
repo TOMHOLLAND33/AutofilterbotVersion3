1b5bbfb87feb67cf8d0ac4d629b041b160e76702
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-10-23 14:26:59 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -120,7 +120,7 @@ async def start(bot, message):
     else:
     	await message.reply_photo(
             photo=f"{random.choice(PHOTO)}",
-            caption=START_MSG.format(message.from_user.mention),
+            caption=START_MSG.format(message.from_user.fisrt_name),
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[
